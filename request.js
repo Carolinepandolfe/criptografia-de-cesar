4bfec4e4767374076f32f374a3456d6da921dac4
Caroline Pandolfe <carolinepandolfe@yahoo.com.br>
2020-04-01 19:24:20 -0300

    Decifrar frase

--- request.js
+++ request.js
@@ -1,45 +1,48 @@
 const axios = require("axios");
-const request = require("request");
 const fs = require("fs");
-const token = "07486efd789916558eba8a6aa60975434a34b87e"
+const sha1 = require('sha1');
+
+const api = "https://api.codenation.dev/v1/challenge/dev-ps/generate-data?token=";
+const token = "07486efd789916558eba8a6aa60975434a34b87e";
 
 
+const decifrar = (cifrado, casa) => {
+    let decifrado = '';
 
-// request(`https://api.codenation.dev/v1/challenge/dev-ps/generate-data?token=${token}`, function ( response, body) {
-//   console.log('body:', body); // Print the HTML for the Google homepage.
+    for (palavra of cifrado) {
+    const unicode = palavra.charCodeAt();
 
-//   fs.writeFileSync('answer.json', JSON.stringify(response.data));
-// });
+    if ((unicode >= 65 && unicode <= 90) || (unicode >= 97 && unicode <= 122)) {
+        let code = unicode - casa
 
+        if (code < 65 || code < 97) {
+            let newCode = 26 - casa + unicode
+            decifrado = decifrado.concat(String.fromCharCode(newCode))
+        } else {
+            decifrado = decifrado.concat(String.fromCharCode(code))
+        }
+    } else {
+        decifrado = decifrado.concat(palavra)
+    }
+}
+    return decifrado;
+}
 
 async function reqHttp(){
-    const res = await axios.get(`https://api.codenation.dev/v1/challenge/dev-ps/generate-data?token=${token}`);
+    const res = await axios.get(`${api}${token}`);
 
-    console.log(res.data)
+    const exist = fs.existsSync('answer.json');
 
-    fs.writeFileSync('answer.json', JSON.stringify(res.data));
-    return 
+        if(!exist){
+            fs.appendFileSync('answer.json', JSON.stringify(res.data));
+        }
+        const fraseDecifrada = decifrar(res.data.cifrado, res.data.numero_casas);
+
+        res.data.decifrado = fraseDecifrada;
+
+        res.data.resumo_criptografico = sha1(fraseDecifrada);
+
+        fs.writeFileSync('answer.json', JSON.stringify(res.data));
 } 
 
 reqHttp()
-
-// let jsonContent = JSON.stringify(reqHttp);
-// console.log(jsonContent);
-
-// let  jsonObj = JSON.parse(jsonContent);
-// console.log(jsonObj);
-
-
-// const reqHttp = axios.get('https://api.codenation.dev/v1/challenge/dev-ps/generate-data?token=07486efd789916558eba8a6aa60975434a34b87e')
-//     .then(function (res){
-//         console.log(res.data);
-//     })
-//         .catch(function(error){
-//             if (error){
-//                 console.log(error)
-//             }
-//         });
-
-
-
-
